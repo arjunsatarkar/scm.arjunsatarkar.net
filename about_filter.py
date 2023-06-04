eb3b1aac839f3629d494f05d6ffb29834ffc464f
Arjun Satarkar <me@arjunsatarkar.net>
2023-06-04 15:43:51 +0530

    Format about_filter.py

--- about_filter.py
+++ about_filter.py
@@ -7,27 +7,29 @@ filename = sys.argv[1]
 result = None
 
 if filename.endswith(".adoc"):
-	result = subprocess.check_output(
-	        [
-	            "asciidoctor",
-	            "--safe-mode",
-	            "secure",
-	            "-a",
-	            "showtitle",
-	            "-e",
-	            "-",
-	        ]
-	    ).decode("utf-8")
+    result = subprocess.check_output(
+        [
+            "asciidoctor",
+            "--safe-mode",
+            "secure",
+            "-a",
+            "showtitle",
+            "-e",
+            "-",
+        ]
+    ).decode("utf-8")
 elif filename.endswith(".md"):
-	result = subprocess.check_output(
-		[
-			"pandoc",
-			"-f", "gfm",
-			"-t", "html",
-			"-",
-		]
-	).decode("utf-8")
+    result = subprocess.check_output(
+        [
+            "pandoc",
+            "-f",
+            "gfm",
+            "-t",
+            "html",
+            "-",
+        ]
+    ).decode("utf-8")
 else:
-	result = sys.stdin.read()
+    result = sys.stdin.read()
 
 print(result)
